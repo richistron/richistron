ac7115d12d3cbd6d97b82441bd1d4b57a05d2319
Ricardo Rivas G <richistron@gmail.com>
2013-10-20 11:21:33 -0500

    close #4
    grunt-bump added and congigurated

--- Gruntfile.js
+++ Gruntfile.js
@@ -307,7 +307,22 @@ module.exports = function (grunt) {
             all: {
                 rjsConfig: '<%= yeoman.app %>/scripts/main.js'
             }
-        }
+        },
+	bump: {
+        options: {
+            files: ['package.json' , 'bower.json'],
+            updateConfigs: [],
+            commit: true,
+            commitMessage: 'Release v%VERSION%',
+            commitFiles: ['package.json' , 'bower.json'], // '-a' for all files
+            createTag: true,
+            tagName: 'v%VERSION%',
+            tagMessage: 'Version %VERSION%',
+            push: true,
+            pushTo: 'upstream',
+            gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
+        }		
+	}
     });
 
     grunt.registerTask('server', function (target) {
